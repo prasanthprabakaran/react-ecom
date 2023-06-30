c6e553a8d7820727570dd268705448722c6770f4
Prasanth P <iprasanthpraba@gmail.com>
2023-06-30 22:33:00 +0530

    build commit

---
+++ src/features/order/Order.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { UseSelector, useDispatch } from 'react-redux';
+import {} from './OrderSlice';
+
+const Order = () => {
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <div>{}</div>
+        </div>
+    )
+}
+export default Order;
